78aacee3077e789ae66bc42c8ee5ae1c93deb5a7
LennyLizowzskiy <lizowzskiy@gmail.com>
2023-12-01 23:43:36 +0300

    fix

--- modules/home-manager/tofi/default.nix
+++ modules/home-manager/tofi/default.nix
@@ -6,7 +6,7 @@ let
   cfg = config.programs.tofi;
 
   generateNoHeadIni = (attrs: 
-    (generators.toINIWithGlobalSection { mkSectionName = (name: "# ${name}") } { globalSection = attrs; })
+    (generators.toINIWithGlobalSection { mkSectionName = (name: "# ${name}"); } { globalSection = attrs; })
   );
 in
 {
